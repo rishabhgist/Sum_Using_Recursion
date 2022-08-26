049c99184c09190fec848b4ed3cb61d87247afeb
Rishabh <rishabhgupta7199@gmail.com>
2022-08-26 19:00:57 +0530

    added sumOfNumber method

--- src/main/java/com/sum/Main.java
+++ src/main/java/com/sum/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public int sumOfNumber(int number) {
+        return number;
+    }
 }
