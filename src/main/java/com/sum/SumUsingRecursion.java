e4e5737da121f7e8d24cb9d5bd8df183ae863d50
Rishabh <rishabhgupta7199@gmail.com>
2022-08-26 19:09:52 +0530

    formatted and added javadoc

--- src/main/java/com/sum/SumUsingRecursion.java
+++ src/main/java/com/sum/SumUsingRecursion.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SumUsingRecursion {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number to get sum till 0");
+        System.out.println("Enter a number to get sum till number");
         int number = input.nextInt();
 
         SumUsingRecursion obj = new SumUsingRecursion();
@@ -15,6 +15,12 @@ public class SumUsingRecursion {
 
     }
 
+    /**
+     * If the number is 0, return 0, otherwise return the number plus the sum of the number minus 1.
+     *
+     * @param number The number to be summed up.
+     * @return The sum of all the numbers from 0 to the number passed in.
+     */
     public int sumOfNumber(int number) {
         if (number == 0) {
             return number;
