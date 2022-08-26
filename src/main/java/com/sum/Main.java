0bec776aba5d95fdf6d55c6115fcffd400ddb454
Rishabh <rishabhgupta7199@gmail.com>
2022-08-26 19:03:31 +0530

    added Scanner class

--- src/main/java/com/sum/Main.java
+++ src/main/java/com/sum/Main.java
@@ -1,11 +1,17 @@
 package com.sum;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
     }
 
     public int sumOfNumber(int number) {
-        return number;
+        if (number == 0) {
+            return number;
+        }
+        return number + sumOfNumber(number - 1);
     }
 }
