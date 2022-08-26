e80cf73d125964d0ea370c2a6ef2a352a4a36dc8
Rishabh <rishabhgupta7199@gmail.com>
2022-08-26 19:06:42 +0530

    added SumUsingRecursion class object and initialized

--- src/main/java/com/sum/SumUsingRecursion.java
+++ src/main/java/com/sum/SumUsingRecursion.java
@@ -2,11 +2,13 @@ package com.sum;
 
 import java.util.Scanner;
 
-public class Main {
+public class SumUsingRecursion {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number to get sum till 0");
         int number = input.nextInt();
+
+        SumUsingRecursion obj = new SumUsingRecursion();
 
     }
 
